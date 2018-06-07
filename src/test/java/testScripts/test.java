c8a81f69437c7f2fe2ed49dbb654ed03dd76e594
jainarun88 <32007198+jainarun88@users.noreply.github.com>
2018-06-07 17:54:24 +0530

    Update test.java

--- src/test/java/testScripts/test.java
+++ src/test/java/testScripts/test.java
@@ -3,6 +3,6 @@ package testScripts;
 public class test {
 
 public static void main (String [] args){
-  system.out.println("Hello")
-
+  system.out.println("Hello111111111111")
+system.out.println("Hello111111111111")
 }
