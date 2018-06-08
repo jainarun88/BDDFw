15c0386d25ed6d7f74feca829f1975cec8210041
Arun Jain <arun.jain.vit@gmail.com>
2018-06-08 16:01:45 +0530

    update commit 08/06/18

--- src/test/java/testScripts/test.java
+++ src/test/java/testScripts/test.java
@@ -2,11 +2,10 @@ package testScripts;
 
 public class test {
 
+	public static void main (String [] args){
 
-public static void main (String [] args){
-
-  System.out.println("Hello1111111112113131");
-  System.out.println("He");
-}
+		System.out.println("Hello1111111112113131");
+		System.out.println("He");
+	}
 
 }
